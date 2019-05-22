10672f27642c0b88177495575e4de7a8ac6e447c
Anthony Diorio <adiorio@e1z3r5p18.42.us.org>
2019-05-21 21:31:40 -0700

    Header is finished with added props, and player section is finised with added props.

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -4,7 +4,7 @@
 //     'My First React Element!'
 // );
 
-const title = <h1>My First React Elment!</h1>;
+// const title = <h1>My First React Elment!</h1>;  JSX
 
 
 // const desc = React.createElement(
@@ -13,10 +13,10 @@ const title = <h1>My First React Elment!</h1>;
 //     'I just learned hot to create a React node and render it into the DOM.'
 // );
 
-const desc = <p>I just learned hot to create a React node and render it into the DOM.</p>;
+// const desc = <p>I just learned hot to create a React node and render it into the DOM.</p>;   JSX
 
-const myTitleID = 'main-title';
-const name = 'Anthony'
+// const myTitleID = 'main-title'; JSX
+// const name = 'Anthony'; JSX
 
 
 // const header = React.createElement(
@@ -33,15 +33,63 @@ const name = 'Anthony'
 //     </header>
 // );
 
-const header = (
-    <header>
-        <h1 id={myTitleID}>{ name }'s First React Element!</h1>
-        <p>{ desc }</p>
-    </header>
-);
+// const header = (
+//     <header>
+//         <h1 id={myTitleID}>{ name }'s First React Element!</h1>
+//         <p>{ desc }</p>
+//     </header>
+// );
+
+
+
+
+
+const Header = (props) => {
+    return (
+        <header>
+            <h1>{ props.title }</h1>
+            <span className="stats">Players: { props.totalPlayer }</span>
+        </header>
+    );
+}
+
+const Player = (props) => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                { props.name }
+            </span>
+
+            <Counter score={ props.score } />
+        </div>
+    );
+}
+
+
+const Counter = (props) => {
+    return (
+        <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">{ props.score }</span>
+        <button className="counter-action increment"> + </button>
+    </div>
+    );
+}
+
+const App = () => {
+    return (
+        <div className="scoreboard">
+            <Header title="Scoreboard" totalPlayer={1} />
+
+            {/* Players List */}
+            <Player name="Anthony" score={50} />
+        </div>
+    );
+}
+
 
 ReactDOM.render(
-    header,
+    <App />,
     document.getElementById('root')
 );
 
